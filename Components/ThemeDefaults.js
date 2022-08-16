2c02fa87c4426e34f3b3d252188a197176bb2385
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2022-08-16 23:40:58 +0800

    Recruiter Registration form 1 & 2

---
+++ Components/ThemeDefaults.js
@@ -0,0 +1,9 @@
+export default {
+    themeBlue: '#2080ff',
+    themeDarkBlue: '#0b132b',
+    themeLighterBlue: '#1c2541',
+    themeOrange: '#FF803C',
+    themeWhite: '#fff',
+    themeBlack: '#000',
+    appIcon: '#E3492B',
+}
