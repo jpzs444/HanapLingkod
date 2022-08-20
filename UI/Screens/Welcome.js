72f05beed19c684acde6e06f623bc4754cfaee53
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2022-08-20 22:27:51 +0800

    Add OTP verification,  functionality Screen 13-15

--- UI/Screens/Welcome.js
+++ UI/Screens/Welcome.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Image, TouchableOpacity, StatusBar, StyleSheet } from 'react-native';
+import { View, Image, TouchableOpacity, StatusBar, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
 import TText from '../Components/TText';
 import { useNavigation } from '@react-navigation/native';
 import ThemeDefaults from '../Components/ThemeDefaults';
@@ -9,23 +9,27 @@ export default function Welcome() {
     const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-        <TText style={styles.headerTitle}>Welcome to HanapLingkod</TText>
-        <TText style={styles.headersubtitle}>You have successfully registered an account!</TText>
-        <TText style={styles.message}>HanapLingkod is on the process of verifying your account and personal information. It usually takes 1-3 business days</TText>
-        <TText style={styles.message}>For the mean time, you may explore the application and set up your profile</TText>
+    <SafeAreaView style={{flex: 1, height: '100%'}}>
+        <ScrollView style={{flex: 1, height: '100%'}}>
+            <View style={styles.container}>
+                <TText style={styles.headerTitle}>Welcome to HanapLingkod</TText>
+                <TText style={styles.headersubtitle}>You have successfully registered an account!</TText>
+                <TText style={styles.message}>HanapLingkod is on the process of verifying your account and personal information. It usually takes 1-3 business days</TText>
+                <TText style={styles.message}>For the mean time, you may explore the application and set up your profile</TText>
 
-        <Image style={styles.image} source={require('../assets/images/bg-welcome.png')} />
+                <Image style={styles.image} source={require('../assets/images/bg-welcome.png')} />
 
-        <View style={styles.btnContainer}>
-            <TouchableOpacity
-                style={styles.btn}
-                onPress={() => navigation.navigate("Home")}
-            >
-                <TText style={styles.btnText}>Go to Homepage</TText>
-            </TouchableOpacity>
-        </View>
-    </View>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() => navigation.navigate("Home")}
+                    >
+                        <TText style={styles.btnText}>Go to Homepage</TText>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -33,33 +37,37 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
+        paddingHorizontal: 20,
+        marginTop: '20%'
     },
     headerTitle: {
         fontFamily: 'LexendDeca_Bold',
+        textAlign: 'center',
         fontSize: 30,
         marginBottom: 30
     },
     headersubtitle: {
         fontFamily: 'LexendDeca_SemiBold',
         fontSize: 18,
+        textAlign: 'center',
         marginBottom: 15,
         color: ThemeDefaults.appIcon
     },
     message: {
         textAlign: 'center',
         fontSize: 18,
-        paddingHorizontal: 60,
+        paddingHorizontal: 15,
         marginBottom: 20
     },
     image: {
         width: 350,
         height: 350,
-        marginVertical: 20
+        // marginVertical: 20
     },
     btnContainer: {
-        marginTop: 90,
-        marginBottom: 20,
+        marginTop: '10%',
+        marginBottom: 100,
         width: '100%',
         alignItems: 'center'
     },
