2e56c3a0088e04d01b41ed66a5facd7124233a34
Patrick <tricksap@gmail.com>
2022-11-21 10:39:29 +0800

    chat backend
    -create a new model for conversation and messages
    -routes for conversation nad messages
    -create a socket server

---
+++ Server/Routes/messages.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const Message = require("../Models/Conversation");
+
+//add
+
+router.post("/messages", async (req, res) => {
+  const newMessage = new Message(req.body);
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get
+
+router.get("/messages/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
