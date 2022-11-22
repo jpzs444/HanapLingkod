6810ebb2d135f0fd08684e4844d28cc8481d0887
Patrick <tricksap@gmail.com>
2022-11-22 21:58:12 +0800

    Update Conversations.js
    -create a conversation cheker

--- Server/Routes/Conversations.js
+++ Server/Routes/Conversations.js
@@ -4,13 +4,23 @@ const Conversation = require("../Models/Conversation");
 //new conv
 
 router.post("/conversations", async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
-  });
-
   try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
+    //check if the convo already exist if true send the existing convo id of false create a new convo and return the id
+    const conversation = await Conversation.find({
+      $and: [
+        { members: { $in: [req.body.senderId] } },
+        { members: { $in: [req.body.receiverId] } },
+      ],
+    });
+    if (conversation != 0) {
+      res.status(200).json(conversation);
+    } else {
+      const newConversation = new Conversation({
+        members: [req.body.senderId, req.body.receiverId],
+      });
+      const savedConversation = await newConversation.save();
+      res.status(200).json(savedConversation);
+    }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -31,15 +41,18 @@ router.get("/conversations/:userId", async (req, res) => {
 
 // get conv includes two userId
 
-router.get("/conversations/find/:firstUserId/:secondUserId", async (req, res) => {
-  try {
-    const conversation = await Conversation.findOne({
-      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
-    });
-    res.status(200).json(conversation)
-  } catch (err) {
-    res.status(500).json(err);
+router.get(
+  "/conversations/find/:firstUserId/:secondUserId",
+  async (req, res) => {
+    try {
+      const conversation = await Conversation.findOne({
+        members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+      });
+      res.status(200).json(conversation);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 module.exports = router;
