0919a8e9df0727716285e63db9b510d31abd97c7
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2022-12-03 20:13:52 +0800

    clear admin react app sprint 4

--- admin/root/javascript/script.js
+++ admin/root/javascript/script.js
@@ -1,11 +1,17 @@
 const sideNav = document.querySelector(".side-navigation")
+const leftSide_nav = document.querySelector(".left")
+const mainBody = document.querySelector("body")
 
 const handleOpenSideNav = () => {
     sideNav.style.marginLeft = "0px"
+    leftSide_nav.style.marginLeft = "300px"
+    mainBody.style.marginLeft = "300px"
 }
 
 const handleCloseSideNav = () => {
     sideNav.style.marginLeft = "-300px"
+    leftSide_nav.style.marginLeft = "0px"
+    mainBody.style.marginLeft = "auto"
 }
 
 const handleActionBtnClick = (action) => {
