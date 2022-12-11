9d085cac57c3689762c131a7e2d8c604f3b00d59
Patrick <tricksap@gmail.com>
2022-12-11 17:14:49 +0800

    Update ConflictChecker.js

--- Server/Helpers/ConflictChecker.js
+++ Server/Helpers/ConflictChecker.js
@@ -31,6 +31,29 @@ async function checkConflict(user, workId, endTime) {
         "item.startTime: " + item.startTime + " item.endTime: " + item.endTime
       );
       console.log("startTime: " + startTime + " endtime " + dayjs(endTime));
+      console.log(
+        dayjs(startTime).isBetween(
+          item.startTime,
+          dayjs(item.endTime),
+          null,
+          "[]"
+        )
+      );
+      console.log(
+        dayjs(endTime).isBetween(
+          item.startTime,
+          dayjs(item.endTime),
+          null,
+          "[]"
+        )
+      );
+
+      console.log(
+        dayjs(item.startTime).isBetween(startTime, dayjs(endTime), null, "[]")
+      );
+      console.log(
+        dayjs(item.endTime).isBetween(startTime, dayjs(endTime), null, "[]")
+      );
       console.log("conflict sched");
       // return res.status(400).send("some text");
       return true;
