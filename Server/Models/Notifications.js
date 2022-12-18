c72b43bde4c44d3d0bac9d1942dfb558c44e6e36
Patrick <tricksap@gmail.com>
2022-12-18 14:48:10 +0800

    Update Notifications.js

--- Server/Models/Notifications.js
+++ Server/Models/Notifications.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = require("mongoose").Schema;
 
-const UserNotificationSchema = new Schema({
-  to: { type: String, required: true },
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  userID: String,
-  read: { type: Boolean, default: 0 },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    index: { expires: "43200m" },
+const UserNotificationSchema = new Schema(
+  {
+    to: { type: String, required: true },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    userID: String,
+    read: { type: Boolean, default: 0 },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now(),
+    //   index: { expires: "43200m" },
+    // },
+    //   readAt: { type: Date, required: true, default: Date.now() },
   },
-  //   readAt: { type: Date, required: true, default: Date.now() },
-});
+  { timestamps: true }
+);
 // UserNotificationSchema.index({ createdAt: 1 }, { expireAfterSeconds: 3 });
 
 module.exports = mongoose.model("UserNotification", UserNotificationSchema);
